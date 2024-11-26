87d7558f70eeac87ab74129afa046c8a805bbb55
박수빈 <subin57419@gmail.com>
2024-11-26 12:16:52 +0900

    Bottom Tab icon 생성

--- Front/navigator/MyTabs.tsx
+++ Front/navigator/MyTabs.tsx
@@ -4,7 +4,7 @@ import HomeScreen from "../src/screen/home/HomeScreen";
 import SearchScreen from "../src/screen/search/SearchScreen";
 import CommunityScreen from "../src/screen/community/CommunityScreen";
 import SettingScreen from "../src/screen/setting/SettingScreen";
-
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,10 +12,43 @@ const MyTabs = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
-                <Tab.Screen name="Search" component={SearchScreen}></Tab.Screen>
-                <Tab.Screen name="Community" component={CommunityScreen}></Tab.Screen>
-                <Tab.Screen name="Setting" component={SettingScreen}></Tab.Screen>
+                <Tab.Screen 
+                    name="Home" 
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ color, size}) => (
+                            <Icon name="home-outline" size={size} color={color} />
+                        )
+
+                    }}
+                />
+                <Tab.Screen 
+                    name="Search" 
+                    component={SearchScreen}
+                    options={{
+                        tabBarIcon: ({ color, size}) => (
+                            <Icon name="search-sharp" size={size} color={color} />
+                        )
+                    }}
+                />
+                <Tab.Screen 
+                    name="Community" 
+                    component={CommunityScreen}
+                    options={{
+                        tabBarIcon: ({ color, size}) => (
+                            <Icon name="earth-outline" size={size} color={color}/>
+                        )
+                    }}
+                />
+                <Tab.Screen 
+                    name="Setting" 
+                    component={SettingScreen}
+                    options={{
+                        tabBarIcon: ({ color, size}) => (
+                            <Icon name="menu" size={size} color={color} />
+                        )
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
